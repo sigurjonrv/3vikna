17d0f602ceacac678c27b056ba591d753510473f
Ólafur Helgi <olafur9@gmail.com>
2014-05-07 10:22:12 +0000

    RequestRepo and ModelView Added, changes made to the Controller and Index View, OHJ

--- 3viknaSolutions/3vikna/Repositories/SubtitlesRepo.cs
+++ 3viknaSolutions/3vikna/Repositories/SubtitlesRepo.cs
@@ -14,10 +14,10 @@ namespace _3vikna.Repositories
         {
             if (m_subtitles.Count == 0)
             {
-                m_subtitles.Add(new Subtitles { ID = 1, MediaName = "Hangover 3", YearPublished = "2013" });
-                m_subtitles.Add(new Subtitles { ID = 1, MediaName = "Hangover 3", YearPublished = "2013" });
-                m_subtitles.Add(new Subtitles { ID = 1, MediaName = "Hangover 3", YearPublished = "2013" });
-                m_subtitles.Add(new Subtitles { ID = 1, MediaName = "Hangover 3", YearPublished = "2013" });
+                m_subtitles.Add(new Subtitles { ID = 1, MediaName = "Hangover 3", YearPublished = "2012" });
+                m_subtitles.Add(new Subtitles { ID = 2, MediaName = "Rocky", YearPublished = "0000" });
+                m_subtitles.Add(new Subtitles { ID = 3, MediaName = "The Amzing Spider-Man 2", YearPublished = "2014" });
+                m_subtitles.Add(new Subtitles { ID = 4, MediaName = "Hangover 1", YearPublished = "2010" });
             }
             return m_subtitles;
         }
