48692753b96ee0b5346555a3819add80568e2b29
Ólafur Helgi <olafur9@gmail.com>
2014-05-08 21:33:31 +0000

    DataBase Connected, Tables been added, Querys OHJ

--- 3viknaSolutions/3vikna/Repositories/RequestsRepo.cs
+++ 3viknaSolutions/3vikna/Repositories/RequestsRepo.cs
@@ -8,30 +8,38 @@ namespace _3vikna.Repositories
 {
     public class RequestsRepo
     {
-        private static List<Requests> m_requests = new List<Requests>();
-
-        public static List<Requests> GetAllRequests()
+        private List<Requests> m_requests = new List<Requests>();
+        AppDataContext db = new AppDataContext();
+        public List<Requests> GetAllRequests()
         {
             if (m_requests.Count == 0)
             {
-                m_requests.Add(new Requests { ID = 1, MediaName = "Screen 1", YearPublished = "1999", IsFinished = true});
-                m_requests.Add(new Requests { ID = 2, MediaName = "Scary Movie 2", YearPublished = "2002", IsFinished = true });
-                m_requests.Add(new Requests { ID = 3, MediaName = "Lion King", YearPublished = "1998, IsFinished = true" });
-                m_requests.Add(new Requests { ID = 4, MediaName = "Catch Me If You Can", YearPublished = "2001", IsFinished = true });
-                m_requests.Add(new Requests { ID = 5, MediaName = "X-Men: Days of Future Past", YearPublished = "2014", IsFinished = false });
-                m_requests.Add(new Requests { ID = 6, MediaName = "Bad Neighbours", YearPublished = "2014", IsFinished = false });
-                m_requests.Add(new Requests { ID = 7, MediaName = "Fed Up", YearPublished = "2014", IsFinished = false });
+                m_requests.Add(new Requests { ID = 1, MediaName = "Screen 1", YearPublished = "1999"/*, IsFinished = true*/});
+                m_requests.Add(new Requests { ID = 2, MediaName = "Scary Movie 2", YearPublished = "2002"/*, IsFinished = true */});
+                m_requests.Add(new Requests { ID = 3, MediaName = "Lion King", YearPublished = "1998" /*,IsFinished = true"*/ });
+                m_requests.Add(new Requests { ID = 4, MediaName = "Catch Me If You Can", YearPublished = "2001" /*, IsFinished = true*/ });
+                m_requests.Add(new Requests { ID = 5, MediaName = "X-Men: Days of Future Past", YearPublished = "2014"/*, IsFinished = false*/ });
+                m_requests.Add(new Requests { ID = 6, MediaName = "Bad Neighbours", YearPublished = "2014"/*, IsFinished = false*/ });
+                m_requests.Add(new Requests { ID = 7, MediaName = "Fed Up", YearPublished = "2014"/*, IsFinished = false */});
             }
             return m_requests;
         }
 
-        public static IEnumerable<Requests> GetAllUnFinishedRequests()
+        /*public IEnumerable<Requests> GetAllUnFinishedRequests()
         {
             var result = (from req in m_requests
                           where req.IsFinished == false
                           orderby req.MediaName ascending
                           select req);
             return result;
+        }*/
+
+        public IEnumerable<Requests> GetAllByDate()
+        {
+            var results = (from r in db.Requests
+                           where r.Date == "09-05-2014"
+                           select r);
+            return results;
         }
     }
 }
